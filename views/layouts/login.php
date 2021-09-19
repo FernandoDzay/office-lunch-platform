ef458847c0a72e72922a869cba1875fa2219af0e
FernandoDzay <luisf.dzay@gmail.com>
2021-09-19 13:05:16 -0500

    preparando para produccion

--- views/layouts/login.php
+++ views/layouts/login.php
@@ -12,6 +12,7 @@
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<title>Login</title>
 
+	<meta name="robots" content="noindex, nofollow"/>
 	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
 	<link rel="icon" href="/favicon.ico" type="image/x-icon">
 	<link rel="stylesheet" href="/css/main.css">
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -13,6 +13,7 @@
 	<title>Inicio</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta name="robots" content="noindex, nofollow"/>
 	<link rel="stylesheet" href="./css/main.css">
 	<link rel="stylesheet" href="./css/custom.css">
 
